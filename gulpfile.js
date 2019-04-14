ecc83931e6320fab67e6dcdeb6a6b816e2d206b4
Glacial <glacial@gmail.com>
2019-04-14 17:03:34 -0400

    Add gulp and npm tasks

--- gulpfile.js
+++ gulpfile.js
@@ -133,7 +133,7 @@ function copy(src, dest) {
 // ------------------------------
 function liveServer(path, proxy) {
     let options = proxy ? {
-        proxy: backend.live
+        proxy: backend.proxy
     } : {
         server: {
             baseDir: path
@@ -141,7 +141,7 @@ function liveServer(path, proxy) {
     };
     browserSync.init(options);
     gulp.watch(frontend.src).on('change', gulp.series('frontend:develop', liveReload));
-    gulp.watch(backend.src).on('change', gulp.series('backend:develop', liveReload));
+    gulp.watch(backend.src).on('change', gulp.series(liveReload));
 };
 
 
@@ -249,84 +249,44 @@ gulp.task('frontend:start', gulp.series('frontend:server'));
 // * Create a WordPress database
 //
 
+// 4.1 - Backend paths
 const backend = new function () {
-    this.url = 'https://wordpress.org';
-    this.version = 'latest.zip';
-    this.proxy = 'http://localhost:8888';
     this.root = 'back-end/';
-    this.src = this.root + 'src/';
+    this.src = this.root + 'src/' + projectConfig.name;
     this.dist = this.root + 'dist/';
-    this.server = this.root + 'server/';
-    this.tmp = this.root + 'tmp/';
-    this.themeName = projectConfig.name;
-    this.themeFolder = this.server + 'wp-content/themes/' + this.themeName;
-    this.live = this.proxy + '/' + this.themeName + '/' + this.server;
+    this.proxy = 'http://localhost:8000';
 };
 
-// 4.1 - Download Wordpress
-function wpDownload() {
-    return (
-        download(backend.url + '/' + backend.version)
-        .pipe(gulp.dest(backend.tmp))
-    )
-};
+// 4.2 - Clean
+// ------------------------------
+gulp.task('backend:clean', () => del(backend.root + '**/*'));
 
-// 4.2 - Decompress Wordpress and add to server folder
-function wpUnzip() {
-    return (
-        gulp
-        .src(backend.tmp + '/*.{tar,tar.bz2,tar.gz,zip}')
-        .pipe(decompress({
-            strip: 1
-        }))
-        .pipe(gulp.dest(backend.server))
-    )
-};
 
 // 4.3 - Rename index files to php
+// ------------------------------
 function backendRename() {
-    return (
-        gulp
-        .src(backend.tmp + '/**/*.html')
+    let path = backend.src
+
+    gulp.src(path + '/**/*.html')
         .pipe(rename(function (file) {
             file.extname = ".php";
         }))
-        .pipe(gulp.dest(backend.tmp))
-    )
+        .pipe(gulp.dest(path))
+
+    return del(path + '**/*.html')
 };
 
-// 4.4 - Install Backend
+// 4.4 - Backed development 
+// ------------------------------
 gulp.task('backend:install', gulp.series(
-    'frontend:develop',
-    () => del(backend.tmp),
-    wpDownload,
-    wpUnzip,
-    () => del(backend.tmp),
-    () => copy(frontend.dist + '/**/*.*', backend.tmp),
-    backendRename,
-    () => del(backend.tmp + '**/*.html'),
-    () => copy(backend.tmp + '/**/*.*', backend.src),
-    () => copy(backend.tmp + '/**/*.*', backend.themeFolder),
-    () => del(backend.tmp)
+    () => copy(frontend.dist + '/**/*.*', backend.src),
+    backendRename
 ));
-
-// 4.4 - Install Backend
-gulp.task('backend:develop', gulp.series(
-    () => del(backend.themeFolder),
-    () => copy(backend.src + '/**/*.*', backend.themeFolder),
-));
-
 
 // 4.5 - Start Backend
+// ------------------------------
 gulp.task('backend:start', gulp.series(
-    'backend:develop',
-    () => liveServer(backend.base, backend.proxy)
-));
-
-// 4.6 - Build Backend
-gulp.task('backend:build', gulp.series(
-    () => del(backend.dist),
-    () => copy(backend.themeFolder + '/**/*.*', backend.dist + 'wp-content/themes/' + backend.themeName)
+    () => liveServer(backend.dist, backend.proxy),
 ));
 
 
@@ -342,25 +302,25 @@ gulp.task('backend:build', gulp.series(
 // this file WILL NOT BE on version control.
 //
 
-function ftpDeploy(param) {
-    let credentials = require('./credentials.json');
-    var conn = vinyl_ftp.create({
-        host: credentials.host,
-        user: credentials.user,
-        password: credentials.password,
-        parallel: credentials.parallel,
-        log: credentials.log
-    });
-    console.log('Uploading ' + param + ' files ...');
-    let globs = [
-        param + '**/*.*',
-    ];
-    var options = {
-        buffer: false
-    }
-    return gulp.src(globs, options)
-        .pipe(conn.newer(credentials.remoteFolder)) // only upload newer files
-        .pipe(conn.dest(credentials.remoteFolder));
-}
-gulp.task('frontend:deploy', gulp.series('frontend:build', () => ftpDeploy(frontend.dist)));
-gulp.task('backend:deploy', gulp.series('backend:build', () => ftpDeploy(backend.dist)));
+// function ftpDeploy(param) {
+//     let credentials = require('./credentials.json');
+//     var conn = vinyl_ftp.create({
+//         host: credentials.host,
+//         user: credentials.user,
+//         password: credentials.password,
+//         parallel: credentials.parallel,
+//         log: credentials.log
+//     });
+//     console.log('Uploading ' + param + ' files ...');
+//     let globs = [
+//         param + '**/*.*',
+//     ];
+//     var options = {
+//         buffer: false
+//     }
+//     return gulp.src(globs, options)
+//         .pipe(conn.newer(credentials.remoteFolder)) // only upload newer files
+//         .pipe(conn.dest(credentials.remoteFolder));
+// }
+// gulp.task('frontend:deploy', gulp.series('frontend:build', () => ftpDeploy(frontend.dist)));
+// gulp.task('backend:deploy', gulp.series('backend:build', () => ftpDeploy(backend.dist)));
