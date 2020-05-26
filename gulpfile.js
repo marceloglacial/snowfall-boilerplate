569ad0abac392aa14c02224e8d7caad6fa41abc7
Marcelo Glacial <glacial@gmail.com>
2020-05-26 17:51:59 -0400

    update npm, webpack and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,21 @@
 // ------------------------------
 const projectConfig = require('./package.json');
 const gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    del = require('del'),
-    autoprefixer = require('gulp-autoprefixer'),
-    imagemin = require('gulp-imagemin'),
-    pug = require('gulp-pug'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    util = require('gulp-util'),
-    vinyl_ftp = require('vinyl-ftp');
-
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync'),
+  del = require('del'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  uglify = require('gulp-uglify'),
+  vinyl_ftp = require('vinyl-ftp'),
+  pug = require('gulp-pug');
 
 // 1.2 - Global Paths
 // ------------------------------
 const all = '**/*.*',
-    folders = '**/*'
-
+  folders = '**/*';
 
 // ==============================================================
 // 2. Functions
@@ -32,98 +29,95 @@ const all = '**/*.*',
 // 2.1 - Clean folder
 // ------------------------------
 function clean(path) {
-    return del(path);
-};
-
+  return del(path);
+}
 
 // 2.2 - Complie SASS
 // ------------------------------
 function styles(src, dest) {
-    return gulp
-        .src(src)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .on('error', sass.logError)
-        .pipe(autoprefixer({
-            browsers: ['last 3 versions'],
-        }))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest(dest))
-};
-
+  return gulp
+    .src(src)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+      })
+    )
+    .on('error', sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: ['last 3 versions'],
+      })
+    )
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(gulp.dest(dest));
+}
 
 // 2.3 - Minimize Scripts
 // ------------------------------
 function scripts(src, dist) {
-    return (
-        gulp
-        .src(src, {
-            sourcemaps: true
-        })
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest(dist))
-    );
-};
-
+  return gulp
+    .src(src, {
+      sourcemaps: true,
+    })
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(gulp.dest(dist));
+}
 
 // 2.4 - Optmize images
 // ------------------------------
 function images(src, dest) {
-    return (
-        gulp
-        .src(src)
-        .pipe(imagemin())
-        .pipe(gulp.dest(dest))
-    )
-};
-
+  return gulp.src(src).pipe(imagemin()).pipe(gulp.dest(dest));
+}
 
 // 2.5 - Complie Handlebars templates
 // ------------------------------
 function templates(templates, dest) {
-    return gulp.src(templates)
-        .pipe(pug())
-        .pipe(gulp.dest(dest))
-};
+  return gulp.src(templates).pipe(pug()).pipe(gulp.dest(dest));
+}
 
 // 2.6 - Copy
 // ------------------------------
 function copy(src, dest) {
-    return gulp.src(src)
-        .pipe(gulp.dest(dest));
-};
-
+  return gulp.src(src).pipe(gulp.dest(dest));
+}
 
 // 2.7 - Start server
 // ------------------------------
 function liveServer(path, proxy) {
-    let options = proxy ? {
-        proxy: backend.proxy
-    } : {
+  let options = proxy
+    ? {
+        proxy: backend.proxy,
+      }
+    : {
         server: {
-            baseDir: path
-        }
-    };
-    browserSync.init(options);
-    gulp.watch(frontend.src).on('change', gulp.series('frontend:develop', liveReload));
-    gulp.watch(backend.src).on('change', gulp.series('backend:develop', liveReload));
-};
-
+          baseDir: path,
+        },
+      };
+  browserSync.init(options);
+  gulp
+    .watch(frontend.src)
+    .on('change', gulp.series('frontend:develop', liveReload));
+  gulp
+    .watch(backend.src)
+    .on('change', gulp.series('backend:develop', liveReload));
+}
 
 // 2.8 - Reload page
 // ------------------------------
 function liveReload() {
-    browserSync.reload();
-};
-
+  browserSync.reload();
+}
 
 // =============================================================
 // 3. Front-end
@@ -131,18 +125,18 @@ function liveReload() {
 
 // 3.1 - Paths
 // ------------------------------
-const frontend = new function () {
-    this.root = 'front-end/';
-    this.all = this.root + all;
-    this.src = this.root + 'src/';
-    this.dist = this.root + 'dist/';
-    this.assets = this.src + 'assets/' + folders;
-    this.vendors = this.src + 'vendors/' + folders;
-    this.styles = this.src + 'styles/**/*.scss';
-    this.scripts = this.src + 'scripts/**/*.js';
-    this.images = this.src + 'images/' + folders;
-    this.templates = this.src + 'templates/*.pug';
-};
+const frontend = new (function () {
+  this.root = 'front-end/';
+  this.all = this.root + all;
+  this.src = this.root + 'src/';
+  this.dist = this.root + 'dist/';
+  this.assets = this.src + 'assets/' + folders;
+  this.vendors = this.src + 'vendors/' + folders;
+  this.styles = this.src + 'styles/**/*.scss';
+  this.scripts = this.src + 'scripts/**/*.js';
+  this.images = this.src + 'images/' + folders;
+  this.templates = this.src + 'templates/*.pug';
+})();
 
 // 3.2 - Assets
 // ------------------------------
@@ -150,27 +144,39 @@ gulp.task('frontend:assets', () => copy(frontend.assets, frontend.dist));
 
 // 3.3 - Vendors
 // ------------------------------
-gulp.task('frontend:vendors', () => copy(frontend.vendors, frontend.dist + 'assets/'));
+gulp.task('frontend:vendors', () =>
+  copy(frontend.vendors, frontend.dist + 'assets/')
+);
 
 // 3.4 - Styles
 // ------------------------------
-gulp.task('frontend:styles', () => styles(frontend.styles, frontend.dist + 'assets/css/'));
+gulp.task('frontend:styles', () =>
+  styles(frontend.styles, frontend.dist + 'assets/css/')
+);
 
 // 3.5 - Scripts
 // ------------------------------
-gulp.task('frontend:scripts', () => scripts(frontend.scripts, frontend.dist + '/assets/js/'));
+gulp.task('frontend:scripts', () =>
+  scripts(frontend.scripts, frontend.dist + '/assets/js/')
+);
 
 // 3.6 - Images
 // ------------------------------
-gulp.task('frontend:images', () => images(frontend.images, frontend.dist + 'assets/img/'));
+gulp.task('frontend:images', () =>
+  images(frontend.images, frontend.dist + 'assets/img/')
+);
 
 // 3.7 - Templates
 // ------------------------------
-gulp.task('frontend:templates', () => templates(frontend.templates, frontend.dist));
+gulp.task('frontend:templates', () =>
+  templates(frontend.templates, frontend.dist)
+);
 
 // 3.8 - HTML
 // ------------------------------
-gulp.task('frontend:html', () => html(frontend.dist + '/**/*.html', frontend.dist));
+gulp.task('frontend:html', () =>
+  html(frontend.dist + '/**/*.html', frontend.dist)
+);
 
 // 3.9 - Clean build files
 // ------------------------------
@@ -178,90 +184,99 @@ gulp.task('frontend:clean', () => clean(frontend.dist));
 
 // 3.10 - Build
 // ------------------------------
-gulp.task('frontend:build',
-    gulp.series(
-        'frontend:clean',
-        'frontend:assets',
-        'frontend:vendors',
-        'frontend:styles',
-        'frontend:scripts',
-        'frontend:images',
-        'frontend:templates'
-    )
+gulp.task(
+  'frontend:build',
+  gulp.series(
+    'frontend:clean',
+    'frontend:assets',
+    'frontend:vendors',
+    'frontend:styles',
+    'frontend:scripts',
+    'frontend:images',
+    'frontend:templates'
+  )
 );
 
 // 3.11 - Develop
 // ------------------------------
-gulp.task('frontend:develop',
-    gulp.series(
-        'frontend:clean',
-        'frontend:assets',
-        'frontend:vendors',
-        'frontend:styles',
-        'frontend:scripts',
-        () => copy(frontend.images, frontend.dist + 'assets/img/'),
-        'frontend:templates'
-    )
+gulp.task(
+  'frontend:develop',
+  gulp.series(
+    'frontend:clean',
+    'frontend:assets',
+    'frontend:vendors',
+    'frontend:styles',
+    'frontend:scripts',
+    () => copy(frontend.images, frontend.dist + 'assets/img/'),
+    'frontend:templates'
+  )
 );
 
 // 3.12 - Start Server
 // ------------------------------
-gulp.task('frontend:server', gulp.series('frontend:develop', () => liveServer(frontend.dist)));
+gulp.task(
+  'frontend:server',
+  gulp.series('frontend:develop', () => liveServer(frontend.dist))
+);
 gulp.task('frontend:start', gulp.series('frontend:server'));
-
-
 
 // =============================================================
 // 4. Back-end
 // =============================================================
 
 // 4.1 - Backend paths
-const backend = new function () {
-    this.root = 'back-end/';
-    this.src = this.root + 'src/';
-    this.dist = this.root + 'dist/' + projectConfig.name;
-    this.proxy = 'http://localhost:8000';
-};
+const backend = new (function () {
+  this.root = 'back-end/';
+  this.src = this.root + 'src/';
+  this.dist = this.root + 'dist/themes/' + projectConfig.name;
+  this.proxy = 'http://localhost:8000';
+})();
 
 // 4.2 - Rename index files to php
 // ------------------------------
 function backendRename() {
-    let path = backend.src
+  let path = backend.src;
 
-    gulp.src(path + '/**/*.html')
-        .pipe(rename(function (file) {
-            file.extname = ".php";
-        }))
-        .pipe(gulp.dest(path))
+  gulp
+    .src(path + '/**/*.html')
+    .pipe(
+      rename(function (file) {
+        file.extname = '.php';
+      })
+    )
+    .pipe(gulp.dest(path));
 
-    return del(path + '**/*.html')
-};
+  return del(path + '**/*.html');
+}
 
-// 4.3 - Backed Install 
+// 4.3 - Backed Install
 // ------------------------------
-gulp.task('backend:install', gulp.series(
+gulp.task(
+  'backend:install',
+  gulp.series(
     () => copy(frontend.dist + '/**/*.*', backend.src),
     backendRename,
-    () => copy(backend.src + '/**/*.*', backend.dist),
-));
+    () => copy(backend.src + '/**/*.*', backend.dist)
+  )
+);
 
 // 4.5 - Start Backend
 // ------------------------------
-gulp.task('backend:start', gulp.series(
-    'backend:install',
-    () => liveServer(backend.dist, backend.proxy),
-));
+gulp.task(
+  'backend:start',
+  gulp.series('backend:install', () => liveServer(backend.dist, backend.proxy))
+);
 
 // 4.6 - Start Develop
 // ------------------------------
-gulp.task('backend:develop', gulp.series(
-    () => copy(backend.src + '/**/*.*', backend.dist),
-));
+gulp.task(
+  'backend:develop',
+  gulp.series(() => copy(backend.src + '/**/*.*', backend.dist))
+);
 
 // 4.7 - Clean build files
 // ------------------------------
 gulp.task('backend:clean', () => clean(backend.dist));
-
 
 // =============================================================
 // 5. FTP Deploy
@@ -270,30 +285,34 @@ gulp.task('backend:clean', () => clean(backend.dist));
 // Please fill info and rename credentials-sample.json
 // to credentials.json
 //
-// NOTE: 
+// NOTE:
 // Due sensitive information,
 // this file WILL NOT BE on version control.
 
-
 function ftpDeploy(param) {
-    let credentials = require('./credentials.json');
-    var conn = vinyl_ftp.create({
-        host: credentials.host,
-        user: credentials.user,
-        password: credentials.password,
-        parallel: credentials.parallel,
-        log: credentials.log
-    });
-    console.log('Uploading ' + param + ' files ...');
-    let globs = [
-        param + '**/*.*',
-    ];
-    var options = {
-        buffer: false
-    }
-    return gulp.src(globs, options)
-        .pipe(conn.newer(credentials.remoteFolder)) // only upload newer files
-        .pipe(conn.dest(credentials.remoteFolder));
+  let credentials = require('./credentials.json');
+  var conn = vinyl_ftp.create({
+    host: credentials.host,
+    user: credentials.user,
+    password: credentials.password,
+    parallel: credentials.parallel,
+    log: credentials.log,
+  });
+  console.log('Uploading ' + param + ' files ...');
+  let globs = [param + '**/*.*'];
+  var options = {
+    buffer: false,
+  };
+  return gulp
+    .src(globs, options)
+    .pipe(conn.newer(credentials.remoteFolder)) // only upload newer files
+    .pipe(conn.dest(credentials.remoteFolder));
 }
-gulp.task('frontend:deploy', gulp.series('frontend:build', () => ftpDeploy(frontend.dist)));
-gulp.task('backend:deploy', gulp.series(() => ftpDeploy(backend.dist)));
+gulp.task(
+  'frontend:deploy',
+  gulp.series('frontend:build', () => ftpDeploy(frontend.dist))
+);
+gulp.task(
+  'backend:deploy',
+  gulp.series(() => ftpDeploy(backend.dist))
+);
